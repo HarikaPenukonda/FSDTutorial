45cf70212cd950f40f98a5a9bb4e623c6ac34831
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-14 15:43:43 -0600

    sum function

--- javascript/functions/first.js
+++ javascript/functions/first.js
@@ -1,7 +1,16 @@
-function myFunction(){
-    console.log(42)
-}
+let lap1 = 50
+let lap2 = 55
+let lap3 = 45
 
-myFunction()
-myFunction()
-myFunction()
+function sum(){
+    let add = lap1 + lap2 + lap3
+    console.log(add)
+}
+sum()
+// function myFunction(){
+//     console.log(42)
+// }
+
+// myFunction()
+// myFunction()
+// myFunction()
