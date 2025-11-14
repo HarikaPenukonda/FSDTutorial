3bdd26654bc2617a4a70531769699a43ca797fc8
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-13 21:08:37 -0600

    dog_age

--- javascript/counter_app/index.js
+++ javascript/counter_app/index.js
@@ -1,6 +1,11 @@
 // document.getElementById("count-el").innerText = 5
-let count = 0
-console.log(count)
+// let count = 0
+// console.log(count)
 
 let myAge = 30
-console.log(myAge)
+// console.log(myAge)
+
+let humanDogRatio = 2
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
