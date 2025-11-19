00f74c3be8b54da92347a53abe7e0491670d2bca
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-18 22:26:24 -0600

    if-else

---
+++ javascript/blackjack/index.js
@@ -0,0 +1,12 @@
+// let firstCard = 5
+// let secondCard = 10
+
+// let sum = firstCard + secondCard
+
+let age = 15
+
+if(age >= 21){
+    console.log("Welcome!")
+}else{
+    console.log("you cannot enter the club")
+}
