d39c789c92eea08402a7419b3a7ddb82921d62f6
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-19 11:58:06 -0600

    arrays length

---
+++ javascript/functions/arrays.js
@@ -0,0 +1,13 @@
+// arrays - list of ordered items
+
+// 0 indexed
+
+let experience = [
+    "software engineer in statefarm",            
+    "Teaching Assistant in University of Dayton",
+    "Masters in computer science",
+    "Associate software engineer in TechMahindra"
+]
+
+console.log(experience[2])
+console.log(experience.length) // 4
