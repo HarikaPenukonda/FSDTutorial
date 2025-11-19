a8b1f2a14c720c4b14c7cdd0e01874ace3ac5983
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-18 22:42:02 -0600

    if-elseIf

--- javascript/blackjack/index.js
+++ javascript/blackjack/index.js
@@ -1,18 +1,26 @@
-// let firstCard = 5
-// let secondCard = 10
+let firstCard = 15
+let secondCard = 10
 
-// let sum = firstCard + secondCard
+let sum = firstCard + secondCard
 
-
-let age = 101
-
-if(age < 100){
-    console.log("Not Eligible")
-}else if(age === 100){
-    console.log("Here is your birthday card from the King!")
+if(sum <= 20){
+    console.log("Do you want to draw a new card? 🙂")
+}else if(sum === 21){
+    console.log("Wohoo! you've got Blackjack!!! 🥳")
 }else{
-    console.log("Not eligible, you have already recieved one")
+    console.log("You're out of the game 😭")
 }
+
+
+// let age = 101
+
+// if(age < 100){
+//     console.log("Not Eligible")
+// }else if(age === 100){
+//     console.log("Here is your birthday card from the King!")
+// }else{
+//     console.log("Not eligible, you have already recieved one")
+// }
 
 // let age = 15
 
