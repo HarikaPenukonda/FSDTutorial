8b38c6ff09886d81f656b40d35a3374825ef15db
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-20 08:48:12 -0600

    functions that return values

---
+++ javascript/functions/third.js
@@ -0,0 +1,22 @@
+let player1Racetime = 102
+let player2Racetime = 107
+
+function fastestRaceTime(){
+    if(player1Racetime<player2Racetime){
+        return player1Racetime
+    }else if(player2Racetime<player1Racetime){
+        return player2Racetime
+    }else{
+        return player1Racetime
+    }
+}
+
+let fastestPlayer = fastestRaceTime()
+console.log(fastestPlayer)
+
+function totalTime(){
+    return player1Racetime + player2Racetime
+}
+
+let totalRaceTime = totalTime()
+console.log(totalRaceTime)
