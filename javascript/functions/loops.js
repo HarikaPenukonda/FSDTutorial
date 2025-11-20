cfa04d7e83f6ce0973749082170bb51ab0384490
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-20 08:34:46 -0600

    Arrays,loops,DOM

--- javascript/functions/loops.js
+++ javascript/functions/loops.js
@@ -1,5 +1,21 @@
+let greeting = ["hello","my","name","is","Harika"]
+let greetingEl = document.getElementById("greeting-el")
+
+for(let i=0; i<greeting.length; i++){
+    //console.log(greeting[i])
+    // greetingEl.textContent = greeting[i] --> overrides the data and displays only the last item
+    greetingEl.textContent += greeting[i] + " " // to keep intact add the item to the previous item
+}
+
+
 // for(let count = 1; count <= 10; count++){
 //     console.log(count)
+// }
+
+// let cards = [3,7,9]
+
+// for(let i=0; i<cards.length; i++){
+//     console.log(cards[i])
 // }
 
 let experience = [
@@ -10,5 +26,5 @@ let experience = [
 ]
 
 for(let i = 0; i<experience.length; i++){
-    console.log(experience[i])
+    //console.log(experience[i])
 }
