a94397e64f1466ccdf9da117e52259956f1aafb1
HarikaPenukonda <penukondaharika@gmail.com>
2025-11-25 10:38:50 -0600

    objects intro

---
+++ javascript/functions/objects.js
@@ -0,0 +1,13 @@
+// objects - store data in depth
+// key : value pair
+
+let airbnb = {
+    nameOftheListing : "San Guisto Abbey",
+    noOfGuests : 4,
+    bedrooms : 1,
+    isPrivate : true,
+    ammenties : ["poolAccess","laundry","parking space","gym" ]
+}
+
+console.log(airbnb.nameOftheListing)
+console.log(airbnb.ammenties[2])
