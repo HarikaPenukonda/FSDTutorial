65b1b6965e64a47bc597211c0a942ac54fa75b94
HarikaPenukonda <penukondaharika@gmail.com>
2025-12-11 03:15:39 -0600

    push,pop,shift,unshift

---
+++ javascript/functions/practice_3.js
@@ -0,0 +1,20 @@
+let countries = ["Cambodia","Australia","USA","United Kingdom","Norway"]
+countries.pop() // removes the last index
+countries.push("Japan") // pushes the last index
+
+countries.shift() // removes the first index
+countries.unshift("India") // replaces the first index
+
+for (let i = 0; i<countries.length; i++){
+    console.log(countries[i])
+}
+
+
+
+
+// let countries = ["India","Australia","USA","United Kingdom","Japan"]
+
+// for (let i = 0; i<countries.length; i++){
+//     console.log(countries[i])
+// }
+
